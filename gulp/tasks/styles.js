ec48b6f33a8292639311d689039a9a96e818f09b
Puritanic <darktasevski@gmail.com>
2017-09-10 23:08:19 +0200

    Improve perfomance and startup time of tasks

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,46 +1,42 @@
-
-var gulp 		 	= require('gulp'),
+var gulp 		 	= require('gulp'),	
 	lost			= require('lost'),
- 	// gutil 		 	= require('gulp-util'),
-	// postcss 	 	= require('gulp-postcss'),
 	cssnext		 	= require('postcss-cssnext'),
 	rucksackCss		= require('rucksack-css'),
 	fontMagician	= require('postcss-font-magician'),
 	postcssUrl	   	= require('postcss-url'),
-	// plumber			= require('gulp-plumber'),
-	reporter	 	= require("postcss-reporter"),
-	// sourcemaps	 	= require('gulp-sourcemaps'),
-	errorCheck 		= function (err) {
-		plugins.notify.onError({
-			title: "Gulp error in " + err.plugin,
-			message:  err.toString()
-		})(err);
-		plugins.util.beep();
-		plugins.util.log(err);
-	  };
+	reporter	 	= require("postcss-reporter");
 	
-	var plugins = require("gulp-load-plugins")({
-		pattern: ['gulp-*', 'gulp.*'],
-		replaceString: /\bgulp[\-.]/, 
-		lazy: true,
-		camelize: true,
-		scope: ['dependencies', 'devDependencies']
-	  });
-
+var errorCheck 		= function (err) {
+	$.notify.onError({
+		title: "Gulp error in " + err.plugin,
+		message:  err.toString()
+	})(err);
+	$.util.beep();
+	$.util.log(err);
+};
+//  Path Configuration	  
 var paths = {
 	cssSource: './app/assets/styles/main.css',
 	cssDest: './app/temp/styles'
 }
 
+var $ = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*'],
+	replaceString: /\bgulp[\-.]/, 
+	lazy: true,
+	camelize: true,
+	scope: ['dependencies', 'devDependencies']
+});
 
+// Task
 gulp.task('styles', function(){
 
 	return gulp.src(paths.cssSource)
-		.pipe(plugins.plumber(function () {
+		.pipe($.plumber(function () {
 			errorHandler: errorCheck
 		}))
-		.pipe(plugins.sourcemaps.init())
-		.pipe(plugins.postcss([
+		.pipe($.sourcemaps.init())
+		.pipe($.postcss([
 			require('postcss-partial-import')({prefix: '_', extension: '.css'}),
 				postcssUrl(),
 					require('postcss-normalize')({browsers: 'last 2 versions'}),	
@@ -51,9 +47,9 @@ gulp.task('styles', function(){
 										lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
 											reporter()
 			]))
-		.on('error', plugins.util.log, function(err){
+		.on('error', $.util.log, function(err){
 			this.emit('end');
 		})
-  		.pipe(plugins.sourcemaps.write('./'))
+  		.pipe($.sourcemaps.write('./'))
   		.pipe(gulp.dest(paths.cssDest));
 });		
