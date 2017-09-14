18fadd24daa1367ce70664878e4bff27171c51ce
Puritanic <darktasevski@gmail.com>
2017-09-14 13:37:34 +0200

    Fix style for gulp task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,14 @@ var gulp 		 	= require('gulp'),
 	rucksackCss		= require('rucksack-css'),
 	fontMagician	= require('postcss-font-magician');
 
-	
+var $ = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*'],
+	replaceString: /\bgulp[\-.]/, 
+	lazy: true,
+	camelize: true,
+	scope: ['dependencies', 'devDependencies']
+});
+
 var errorCheck 		= function (err) {
 	$.notify.onError({
 		title: "Gulp error in " + err.plugin,
@@ -18,14 +25,6 @@ var paths = {
 	cssDest: './app/temp/styles'
 }
 
-var $ = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*'],
-	replaceString: /\bgulp[\-.]/, 
-	lazy: true,
-	camelize: true,
-	scope: ['dependencies', 'devDependencies']
-});
-
 // Task
 gulp.task('styles', function(){
 
@@ -36,17 +35,15 @@ gulp.task('styles', function(){
 		.pipe($.sourcemaps.init())
 		.pipe($.postcss([
 			require('postcss-partial-import')({prefix: '_', extension: '.css'}),
-				require('postcss-assets')({
-						basePath: 'app'
-					}),
-						require("postcss-url")(),
-							require('postcss-normalize')({browsers: 'last 2 versions'}),	
-								fontMagician(),	// https://github.com/jonathantneal/postcss-font-magician	 		
-									require("postcss-cssnext")(),	// http://cssnext.io/features/
-										rucksackCss(), // http://simplaio.github.io/rucksack/docs/#
-											require('postcss-nesting'),
-												lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
-													require("postcss-reporter")()
+				require('postcss-assets')({ basePath: 'app' }), // assets url handling
+					require("postcss-url")(),
+						require('postcss-normalize')({browsers: 'last 2 versions'}),	
+							fontMagician(),	// https://github.com/jonathantneal/postcss-font-magician	 		
+								require("postcss-cssnext")(),	// http://cssnext.io/features/
+									rucksackCss(), // http://simplaio.github.io/rucksack/docs/#
+										require('postcss-nesting'),
+											lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
+												require("postcss-reporter")()
 			]))
 		.on('error', $.util.log, function(err){
 			this.emit('end');
