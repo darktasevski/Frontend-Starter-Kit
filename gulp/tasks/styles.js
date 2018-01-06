c251b08984298b2fd569a47895649a1c708da100
Puritanic <darktasevski@gmail.com>
2018-01-07 00:37:49 +0100

    Remove lost and handle Sass Resolves #2

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,44 +1,57 @@
-var gulp 		 	= require('gulp'),	
-	lost			= require('lost'),
-	rucksackCss		= require('rucksack-css'),
-	fontMagician	= require('postcss-font-magician'),
-	onError 		= require( '../utilities/errorHandler');
+const gulp = require("gulp");
+const rucksackCss = require("rucksack-css");
+const fontMagician = require("postcss-font-magician");
+const onError = require("../utilities/errorHandler");
 
-var $ = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*'],
-	replaceString: /\bgulp[\-.]/, 
-	lazy: true,
-	camelize: true,
-	scope: ['dependencies', 'devDependencies']
+const $ = require("gulp-load-plugins")({
+  pattern: ["gulp-*", "gulp.*"],
+  replaceString: /\bgulp[\-.]/,
+  lazy: true,
+  camelize: true,
+  scope: ["dependencies", "devDependencies"]
 });
 
-//  Path variables	  
-var paths = {
-	cssSource: './app/assets/styles/main.css',
-	cssDest: './app/temp/styles'
-}
+//  Path variables
+const paths = {
+  cssSource: "./app/assets/styles/main.scss",
+  cssDest: "./app/temp/styles"
+};
 
 // Task
-gulp.task('styles', function(){
-
-	return gulp.src(paths.cssSource)
-		.pipe($.plumber({
-			errorHandler: onError
-		}))
-		.pipe($.sourcemaps.init())
-		.pipe($.postcss([
-			require('postcss-partial-import')({prefix: '_', extension: '.css'}),
-				require('postcss-assets')({ basePath: `app`, loadPaths: ['assets/images'], relative: 'assets/styles'}), // assets url handling
-					require('postcss-normalize')({browsers: 'last 2 versions'}),
-						fontMagician({hosted: ['./app/assets/fonts', './assets/fonts/'], foundries: ['google']}),	// https://github.com/jonathantneal/postcss-font-magician	 		
-							require("postcss-cssnext")(),	// http://cssnext.io/features/
-								rucksackCss(), // http://simplaio.github.io/rucksack/docs/#
-									require('postcss-nesting'),
-										lost(), // lost must be after nesting, so that media queries can work with it http://lostgrid.org/lostgrid-example.html
-											require("css-mqpacker")({sort: true}),
-												require("postcss-reporter")()
-			]))
-  		.pipe($.sourcemaps.write('./'))
-		.pipe($.plumber.stop())
-		.pipe(gulp.dest(paths.cssDest));
-});		
+gulp.task("styles", () =>
+  gulp
+    .src(paths.cssSource)
+    .pipe(
+      $.plumber({
+        errorHandler: onError
+      })
+    )
+    .pipe($.sourcemaps.init())
+    .pipe($.sass().on("error", $.sass.logError))
+    .pipe(
+      $.postcss([
+        require("postcss-partial-import")({
+          prefix: "_",
+          extension: ".css"
+        }),
+        require("postcss-assets")({
+          basePath: "app",
+          loadPaths: ["assets/images"],
+          relative: "assets/styles"
+        }), // assets url handling
+        require("postcss-normalize")({ browsers: "last 2 versions" }),
+        fontMagician({
+          hosted: ["./app/assets/fonts", "./assets/fonts/"],
+          foundries: ["google"]
+        }), // https://github.com/jonathantneal/postcss-font-magician
+        require("postcss-cssnext")(), // http://cssnext.io/features/
+        rucksackCss(), // http://simplaio.github.io/rucksack/docs/#
+        require("postcss-nesting"),
+        require("css-mqpacker")({ sort: true }),
+        require("postcss-reporter")()
+      ])
+    )
+    .pipe($.sourcemaps.write("./"))
+    .pipe($.plumber.stop())
+    .pipe(gulp.dest(paths.cssDest))
+);
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,48 +1,45 @@
-const gulp 			= require('gulp'),
-	  browserSync 	= require('browser-sync').create();
+const gulp = require("gulp"),
+  browserSync = require("browser-sync").create();
 
 var $ = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*'],
-	replaceString: /\bgulp[\-.]/, 
-	lazy: true,
-	scope: ['dependencies', 'devDependencies']
+  pattern: ["gulp-*", "gulp.*"],
+  replaceString: /\bgulp[\-.]/,
+  lazy: true,
+  scope: ["dependencies", "devDependencies"]
 });
 
-gulp.task('watch', function(){
-	
-	$.util.log('---------------------------------------');
-	$.util.log('Night gathers, and now my watch begins.');
-	$.util.log('---------------------------------------');
+gulp.task("watch", function() {
+  $.util.log("---------------------------------------");
+  $.util.log("Night gathers, and now my watch begins.");
+  $.util.log("---------------------------------------");
 
-	browserSync.init({
+  browserSync.init({
     // Customize the Browsersync console logging prefix
-    	logPrefix: ' 💻 ',
-		server: {
-			baseDir: 'app',
-			index: "index.html"
-		},
-		port: 3000
-	});
+    logPrefix: " 💻 ",
+    server: {
+      baseDir: "app",
+      index: "index.html"
+    },
+    port: 3000
+  });
 
-	$.watch('./app/index.html', function(){
-		browserSync.reload();
-	});
+  $.watch("./app/index.html", function() {
+    browserSync.reload();
+  });
 
-	$.watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('cssInject');
-	});
+  $.watch("./app/assets/styles/**/*.+(css|scss)", function() {
+    gulp.start("cssInject");
+  });
 
-	$.watch('./app/assets/scripts/**/*.js', function(){
-		gulp.start('scriptsRefresh');
-	});
+  $.watch("./app/assets/scripts/**/*.js", function() {
+    gulp.start("scriptsRefresh");
+  });
 });
 
-gulp.task('cssInject', ['styles'], function(){
-	return gulp.src('./app/temp/styles/main.css')
-		.pipe(browserSync.stream());
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/main.css").pipe(browserSync.stream());
 });
 
-gulp.task('scriptsRefresh', ['scripts'], function(){
-	browserSync.reload();
-})
-   
+gulp.task("scriptsRefresh", ["scripts"], function() {
+  browserSync.reload();
+});
