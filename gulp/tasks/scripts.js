1bc27c3a70c127d448c7a2941545bf3d953bbc74
Puritanic <darktasevski@gmail.com>
2018-02-08 11:11:03 +0100

    REfactor some tasks and styles, add html templates

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,17 +1,26 @@
 const gulp = require('gulp');
 const webpack = require('webpack');
 const notifier = require('node-notifier');
+const Log = require('../utilities/Log.js');
+const webpackConfig = require('../../webpack.config.js');
 
 gulp.task('scripts', ['modernizr'], (callback) => {
-  webpack(require('../../webpack.config.js'), (err, stats) => {
-    if (stats.compilation.errors.length) {
-      // Notifiers if errors
-      notifier.notify({
-        title: 'Webpack error',
-        message: stats.compilation.errors[0].error.toString('utf8')
-      });
+  webpack(webpackConfig, (err, stats) => {
+    if (err) {
+      new Log('Webpack', err).error();
     }
-    console.log(stats.toString());
+    new Log(
+      'Webpack',
+      stats.toString({
+        assets: true,
+        chunks: false,
+        chunkModules: false,
+        colors: true,
+        hash: false,
+        timings: true,
+        version: false
+      })
+    ).info();
     callback();
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		App: "./app/assets/scripts/App.js", // tell webpack which file should it be looking for to bundle it
-		Vendor: "./app/assets/scripts/Vendor.js"
-	},
-	output: {
-		path: path.resolve(__dirname, "./app/temp/scripts"),
-		filename: "[name].js"
-	},
-	module: {
-		loaders: [{
-			loader: 'babel-loader',
-			query: {
-				presets: [
-					["env", {
-						"targets": {
-							"browsers": ["last 2 versions", "safari >= 7"]
-						}
-					}]
-				]
-			},
-			test: /\.js$/,
-			exclude: /node_modules/
-		}]
-	}
-}
+  entry: {
+    App: './app/assets/scripts/App.js', // tell webpack which file should it be looking for to bundle it
+    Vendor: './app/assets/scripts/Vendor.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './app/temp/scripts'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [
+              'env',
+              {
+                targets: {
+                  browsers: ['last 2 versions', 'safari >= 7']
+                }
+              }
+            ]
+          ]
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
 
 // todo mocha testing after bundling
