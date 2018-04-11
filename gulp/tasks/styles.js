262fabb462a9c91e1e38015b64bc9c2ac8c99672
Darko Tasevski <c.dtasevski@gogoair.com>
2018-04-11 21:05:25 +0200

    Remove postcss nesting (already included in nextcss)

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const fontMagician = require('postcss-font-magician');
 const onError = require('../utilities/errorHandler');
+const postcssImageSet = require('postcss-image-set-polyfill');
 
 const $ = require('gulp-load-plugins')({
   pattern: ['gulp-*', 'gulp.*'],
@@ -20,29 +21,33 @@ const paths = {
 gulp.task('styles', () =>
   gulp
     .src(paths.cssSource)
-    .pipe($.plumber({
-      errorHandler: onError
-    }))
+    .pipe(
+      $.plumber({
+        errorHandler: onError
+      })
+    )
     .pipe($.sourcemaps.init())
     .pipe($.sass().on('error', $.sass.logError))
-    .pipe($.postcss([
-      require('postcss-partial-import')({
-        prefix: '_',
-        extension: '.css'
-      }),
-      require('postcss-assets')({
-        basePath: 'app',
-        loadPaths: ['assets/images']
-      }), // assets url handling
-      require('postcss-normalize')({ browsers: 'last 2 versions' }),
-      fontMagician({
-        hosted: ['./app/assets/fonts', './assets/fonts/'],
-        foundries: ['google']
-      }), // https://github.com/jonathantneal/postcss-font-magician
-      require('postcss-cssnext')(), // http://cssnext.io/features/
-      require('postcss-nesting'),
-      require('postcss-reporter')()
-    ]))
+    .pipe(
+      $.postcss([
+        require('postcss-partial-import')({
+          prefix: '_',
+          extension: '.css'
+        }),
+        require('postcss-assets')({
+          basePath: 'app',
+          loadPaths: ['assets/images'],
+          cachebuster: true
+        }), // assets url handling -- https://github.com/borodean/postcss-assets
+        require('postcss-normalize')({ browsers: 'last 2 versions' }),
+        fontMagician({
+          hosted: ['./app/assets/fonts']
+        }), // https://github.com/jonathantneal/postcss-font-magician
+        require('postcss-cssnext')(), // http://cssnext.io/features/
+        require('postcss-reporter')()
+      ])
+    )
     .pipe($.sourcemaps.write('./'))
     .pipe($.plumber.stop())
-    .pipe(gulp.dest(paths.cssDest)));
+    .pipe(gulp.dest(paths.cssDest))
+);
